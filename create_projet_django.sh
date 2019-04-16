7cdcc40f1ec1403f1c62b4089d6e279b93d46a9a
ChaibM <47309075+ChaibM@users.noreply.github.com>
2019-04-16 18:13:29 +0100

    Update create_projet_django.sh
    version v1.2 work

--- create_projet_django.sh
+++ create_projet_django.sh
@@ -4,17 +4,16 @@ mkdir /commun/helloworld
 cd /commun/helloworld
 
 echo "create a new virtual environnement install django + activated"
+echo " 
+django-admin startproject helloworld_project ."
+echo "lancer le serveur"
+echo "python manage.py migrate python manage.py runserver"
 
 sudo pipenv install django==2.2.0
+echo "activation d'environnement"
 sudo pipenv shell 
 echo "create project"
-django-admin startproject helloworld_project .
 
-echo "lancer le serveur"
-
-python manage.py migrate
-python manage.py runserver
-echo "activation d'environnement"
 
 echo "visit http://127.0.0.1:8000/  "
 
