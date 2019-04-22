9b311c99d6efdb9c3e9573193ec9dcb9d07b04a8
Mouad <Mouadzaza@gmail.com>
2019-04-22 12:41:39 +0100

    add var nom projet

--- create_projet_django.sh
+++ create_projet_django.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-mkdir /commun/helloworld
-cd /commun/helloworld
+nom_projet=crepes_bretonnes
 
-echo "create a new virtual environnement install django + activated"
-echo " 
-django-admin startproject helloworld_project ."
+mkdir /commun/$nom_projet
+cd /commun/$nom_projet
+
+echo "####################create a new virtual environnement install django + activated##################"
+echo "##################################################################" 
+echo "django-admin startproject helloworld_project ."
 echo "lancer le serveur"
-echo "python manage.py migrate python manage.py runserver"
+echo "python manage.py migrate ; python manage.py runserver"
 
 sudo pipenv install django==2.2.0
 echo "activation d'environnement"
